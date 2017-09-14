cf544670b8d95f3f53716023ca665689a05a2784
Philli.pe Casorla Sagot <phillipe@makespace.com>
2017-09-14 09:53:08 -0600

    Support Swift4

--- Mockingjay/NSURLSessionConfiguration.swift
+++ Mockingjay/NSURLSessionConfiguration.swift
@@ -9,30 +9,34 @@
 import Foundation
 
 let swizzleDefaultSessionConfiguration: Void = {
-  let defaultSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.default))
-  let mockingjayDefaultSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(URLSessionConfiguration.mockingjayDefaultSessionConfiguration))
-  method_exchangeImplementations(defaultSessionConfiguration, mockingjayDefaultSessionConfiguration)
-
-  let ephemeralSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeral))
-  let mockingjayEphemeralSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(URLSessionConfiguration.mockingjayEphemeralSessionConfiguration))
-  method_exchangeImplementations(ephemeralSessionConfiguration, mockingjayEphemeralSessionConfiguration)
+    guard
+        let defaultSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.default)),
+        let mockingjayDefaultSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(URLSessionConfiguration.mockingjayDefaultSessionConfiguration))
+        else { return }
+    method_exchangeImplementations(defaultSessionConfiguration, mockingjayDefaultSessionConfiguration)
+    
+    guard
+        let ephemeralSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(getter: URLSessionConfiguration.ephemeral)),
+        let mockingjayEphemeralSessionConfiguration = class_getClassMethod(URLSessionConfiguration.self, #selector(URLSessionConfiguration.mockingjayEphemeralSessionConfiguration))
+        else { return }
+    method_exchangeImplementations(ephemeralSessionConfiguration, mockingjayEphemeralSessionConfiguration)
 }()
 
 extension URLSessionConfiguration {
-  /// Swizzles NSURLSessionConfiguration's default and ephermeral sessions to add Mockingjay
-  public class func mockingjaySwizzleDefaultSessionConfiguration() {
-    _ = swizzleDefaultSessionConfiguration
-  }
-
-  class func mockingjayDefaultSessionConfiguration() -> URLSessionConfiguration {
-    let configuration = mockingjayDefaultSessionConfiguration()
-    configuration.protocolClasses = [MockingjayProtocol.self] as [AnyClass] + configuration.protocolClasses!
-    return configuration
-  }
-
-  class func mockingjayEphemeralSessionConfiguration() -> URLSessionConfiguration {
-    let configuration = mockingjayEphemeralSessionConfiguration()
-    configuration.protocolClasses = [MockingjayProtocol.self] as [AnyClass] + configuration.protocolClasses!
-    return configuration
-  }
+    /// Swizzles NSURLSessionConfiguration's default and ephermeral sessions to add Mockingjay
+    @objc public class func mockingjaySwizzleDefaultSessionConfiguration() {
+        _ = swizzleDefaultSessionConfiguration
+    }
+    
+    @objc class func mockingjayDefaultSessionConfiguration() -> URLSessionConfiguration {
+        let configuration = mockingjayDefaultSessionConfiguration()
+        configuration.protocolClasses = [MockingjayProtocol.self] as [AnyClass] + configuration.protocolClasses!
+        return configuration
+    }
+    
+    @objc class func mockingjayEphemeralSessionConfiguration() -> URLSessionConfiguration {
+        let configuration = mockingjayEphemeralSessionConfiguration()
+        configuration.protocolClasses = [MockingjayProtocol.self] as [AnyClass] + configuration.protocolClasses!
+        return configuration
+    }
 }
--- Mockingjay/XCTest.swift
+++ Mockingjay/XCTest.swift
@@ -10,54 +10,57 @@ import ObjectiveC
 import XCTest
 
 let swizzleTearDown: Void = {
-  let tearDown = class_getInstanceMethod(XCTest.self, #selector(XCTest.tearDown))
-  let mockingjayTearDown = class_getInstanceMethod(XCTest.self, #selector(XCTest.mockingjayTearDown))
-  method_exchangeImplementations(tearDown, mockingjayTearDown)
+    guard
+        let tearDown = class_getInstanceMethod(XCTest.self, #selector(XCTest.tearDown)),
+        let mockingjayTearDown = class_getInstanceMethod(XCTest.self, #selector(XCTest.mockingjayTearDown))
+        else { return }
+    method_exchangeImplementations(tearDown, mockingjayTearDown)
 }()
 
 var AssociatedMockingjayRemoveStubOnTearDownHandle: UInt8 = 0
 extension XCTest {
-  // MARK: Stubbing
-
-  /// Whether Mockingjay should remove stubs on teardown
-  public var mockingjayRemoveStubOnTearDown: Bool {
-    get {
-      let associatedResult = objc_getAssociatedObject(self, &AssociatedMockingjayRemoveStubOnTearDownHandle) as? Bool
-      return associatedResult ?? true
+    // MARK: Stubbing
+    
+    /// Whether Mockingjay should remove stubs on teardown
+    public var mockingjayRemoveStubOnTearDown: Bool {
+        get {
+            let associatedResult = objc_getAssociatedObject(self, &AssociatedMockingjayRemoveStubOnTearDownHandle) as? Bool
+            return associatedResult ?? true
+        }
+        
+        set {
+            objc_setAssociatedObject(self, &AssociatedMockingjayRemoveStubOnTearDownHandle, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
     }
-
-    set {
-      objc_setAssociatedObject(self, &AssociatedMockingjayRemoveStubOnTearDownHandle, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    
+    @discardableResult public func stub(_ matcher: @escaping Matcher, delay: TimeInterval? = nil, _ builder: @escaping Builder) -> Stub {
+        if mockingjayRemoveStubOnTearDown {
+            XCTest.mockingjaySwizzleTearDown()
+        }
+        
+        return MockingjayProtocol.addStub(matcher: matcher, delay: delay, builder: builder)
     }
-  }
-
-  @discardableResult public func stub(_ matcher: @escaping Matcher, delay: TimeInterval? = nil, _ builder: @escaping Builder) -> Stub {
-    if mockingjayRemoveStubOnTearDown {
-      XCTest.mockingjaySwizzleTearDown()
+    
+    public func removeStub(_ stub:Stub) {
+        MockingjayProtocol.removeStub(stub)
     }
-
-    return MockingjayProtocol.addStub(matcher: matcher, delay: delay, builder: builder)
-  }
-
-  public func removeStub(_ stub:Stub) {
-    MockingjayProtocol.removeStub(stub)
-  }
-
-  public func removeAllStubs() {
-    MockingjayProtocol.removeAllStubs()
-  }
-
-  // MARK: Teardown
-
-  public class func mockingjaySwizzleTearDown() {
-    _ = swizzleTearDown
-  }
-
-  func mockingjayTearDown() {
-    mockingjayTearDown()
-
-    if mockingjayRemoveStubOnTearDown {
-      MockingjayProtocol.removeAllStubs()
+    
+    public func removeAllStubs() {
+        MockingjayProtocol.removeAllStubs()
     }
-  }
+    
+    // MARK: Teardown
+    
+    public class func mockingjaySwizzleTearDown() {
+        _ = swizzleTearDown
+    }
+    
+    @objc func mockingjayTearDown() {
+        mockingjayTearDown()
+        
+        if mockingjayRemoveStubOnTearDown {
+            MockingjayProtocol.removeAllStubs()
+        }
+    }
 }
+
